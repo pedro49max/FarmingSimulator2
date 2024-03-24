e98c3d0a0bb7e08aa2ed3b0524ba85a01ce330e6
pedro49max <pedrotob@ucm.es>
2024-03-24 12:09:22 +0100

    fill_in_data() of builder.java

--- src/simulator/factories/Builder.java
+++ src/simulator/factories/Builder.java
@@ -24,6 +24,21 @@ public abstract class Builder<T> {
 		return info;
 	}
 	protected void fill_in_data(JSONObject o) {
+		// Add data specific to each type of region builder
+	    switch (_type_tag) {
+	        case "default":
+	            // For default region builder, no additional data needed
+	            break;
+	        case "dynamic":
+	            // For dynamic region builder, include factor and food fields
+	            o.put("factor", "food increase factor (optional, default 2.0)");
+	            o.put("food", "initial amount of food (optional, default 100.0)");
+	            break;
+	        // Add cases for other region builders if needed
+	        default:
+	            // Handle unrecognized type tag
+	            throw new IllegalArgumentException("Unrecognized region builder type: " + _type_tag);
+	    }
 	}
 	@Override
 	public String toString() {
